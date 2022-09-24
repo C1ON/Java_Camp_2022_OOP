4ee7eeb6b6d9f1d9f04b5d68c2daaec69998ea96
Caner <caneron6@gmail.com>
2022-09-24 21:02:38 +0300

    first

---
+++ classesWithAttributes/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Product product = new Product(1,"Laptop","",300,3,"Siyah");
+
+        ProductManager productManager = new ProductManager();
+        productManager.Add(product);
+
+        System.out.println(product.getKod());
+
+
+
+    }
+}
